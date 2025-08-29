f7a829f26692754a7a29c534139b4b18239a27c4
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-08-29 00:53:08 -0400

    options.py

--- options.py
+++ options.py
@@ -20,7 +20,7 @@ df = yf.download(ticker, start, end)
 #define variables
 r=0.01 #risk free rate
 S = df['Close'].iloc[-1] #base price
-K=40 #strike
+K=200 #strike
 T=240/365
 sigma = 0.3 #volatility?
 

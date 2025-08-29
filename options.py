16394e38fc220cbd74d276e212735a3922fee2ea
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-08-29 02:34:02 -0400

    options.py

--- options.py
+++ options.py
@@ -64,14 +64,14 @@ def black_scholes(r, S, K, T, sigma, type = option_type_code):
       st.write("Error:", e)
       return None
       
-    delta = norm.cdf(d1) if option_type.upper() == "C" else norm.cdf(d1)-1
-    gamma = norm.pdf(d1) / (S * sigma * np.sqrt(T))
-    vega = S * norm.pdf(d1) * np.sqrt(T)
-    theta = -(S*norm.pdf(d1)*sigma)/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*norm.cdf(d2) if option_type.upper()=="C" \
-            else -(S*norm.pdf(d1)*sigma)/(2*np.sqrt(T)) + r*K*np.exp(-r*T)*norm.cdf(-d2)
-    rho = K*T*np.exp(-r*T)*norm.cdf(d2) if option_type.upper()=="C" else -K*T*np.exp(-r*T)*norm.cdf(-d2)
+  delta = norm.cdf(d1) if option_type.upper() == "C" else norm.cdf(d1)-1
+  gamma = norm.pdf(d1) / (S * sigma * np.sqrt(T))
+  vega = S * norm.pdf(d1) * np.sqrt(T)
+  theta = -(S*norm.pdf(d1)*sigma)/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*norm.cdf(d2) if option_type.upper()=="C" \
+    else -(S*norm.pdf(d1)*sigma)/(2*np.sqrt(T)) + r*K*np.exp(-r*T)*norm.cdf(-d2)
+  rho = K*T*np.exp(-r*T)*norm.cdf(d2) if option_type.upper()=="C" else -K*T*np.exp(-r*T)*norm.cdf(-d2)
 
-    return price, delta, gamma, vega, theta, rho
+  return price, delta, gamma, vega, theta, rho
 
 # =========================
 # CALCULATE

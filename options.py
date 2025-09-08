0ddf72661cb52fa5f85dff6328631d07e37688dd
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-09-07 22:39:29 -0400

    options.py

--- options.py
+++ options.py
@@ -392,7 +392,7 @@ with tab2:
                                     x=list(range(0, N+1)), 
                                     y=np.arange(opt.shape[0],0,-1)))
     fig_binomial.update_layout(title="Binomial: option values heatmap (time vs node)")
-    st.plotly_chart_binomial(fig, use_container_width=True)
+    st.plotly_chart(fig_binomial, use_container_width=True)
 
 with tab3:
     ticker_input_mc = st.text_input("Enter Ticker", value="AAPL", key="ticker_mc")
